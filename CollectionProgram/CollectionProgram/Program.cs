3da4ec8ab5e51c23e5f82dbce8b86f28c3c5263f
chinki kumari singh <singhchinkikumari.com>
2022-06-10 19:19:27 +0530

    [Chinki] Code Of Collection Demo Part

---
+++ CollectionProgram/CollectionProgram/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CollectionsDemo
+{
+    class Program
+    {
+         static void  Main(string[] args)
+        { 
+            Console.WriteLine("Welcome to C# Collections");
+            doListDemo();
+        }
+        private static void doListDemo()
+        {
+            Console.WriteLine("\nIn doListDemo");
+            List<string> list = new List<string>();
+            list.Add("chinki");
+            list.Add("suraj");
+            list.Add("ravi");
+            list.Add("ram");
+            //Itreating List elements
+            foreach (var element in list)
+            {
+                Console.WriteLine(element);
+            }
+        }
+    }
+
+}
